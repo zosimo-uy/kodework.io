d6caf398cd08ffc1df32a61c35c6f674ee21d13f
ZorsCode <zhosyuy21@gmail.com>
2023-04-01 17:34:00 +0800

    added fix for the second section margin

--- service.js
+++ service.js
@@ -26,3 +26,21 @@ window.onload = function (){
   let contactForm = document.getElementById(`contactForm`);
   contactForm.onsubmit = captureModal;
 };
+
+
+const serviceModal = document.getElementById('serviceRequest')
+serviceModal.addEventListener('show.bs.modal', event => {
+  // Button that triggered the modal
+  const button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  const recipient = button.getAttribute('data-bs-whatever')
+  // If necessary, you could initiate an Ajax request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  const modalTitle = serviceModal.querySelector('.modal-title')
+  const modalBodyInput = serviceModal.querySelector('.modal-body input')
+
+  modalTitle.textContent = `Request for a ${recipient}`
+  modalBodyInput.value = recipient
+})
